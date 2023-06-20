c8b5bc84319b6575ab0330f4e21257f4fae328b9
Игонин Павел <88630513+Pavel7334@users.noreply.github.com>
2023-06-20 10:48:32 +0400

    Регистрация и Авторизация Пользователей

---
+++ __main__.py
@@ -0,0 +1,10 @@
+import uvicorn
+
+if __name__ == '__main__':
+    uvicorn.run(
+        'main:app',
+        reload=True,
+        port=8000,
+        # env_file='.env',
+    )
+
--- main.py
+++ main.py
@@ -6,8 +6,13 @@ from fastapi import FastAPI, Request, status
 from fastapi.encoders import jsonable_encoder
 from fastapi.exceptions import ValidationError
 from fastapi.responses import JSONResponse
+from fastapi_users import fastapi_users, FastAPIUsers
 
 from pydantic import BaseModel, Field
+
+from auth.auth import auth_backend
+from auth.manager import get_user_manager
+from auth.shemas import UserRead, UserCreate
 
 app = FastAPI(
     title="Trading App"
@@ -74,3 +79,21 @@ def add_trades(trades: List[Trade]):
     fake_trades.extend(trades)
     return {"status": 200, "data": fake_trades}
 
+
+fastapi_users = FastAPIUsers[User, int](
+    get_user_manager,
+    [auth_backend],
+)
+
+
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix="/auth/jwt",
+    tags=["auth"],
+)
+
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
+)
