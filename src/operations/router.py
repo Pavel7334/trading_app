6bac2530979d2810a2f8d156ce9ddfb243a145a3
Игонин Павел <88630513+Pavel7334@users.noreply.github.com>
2023-06-23 12:12:18 +0400

    Фоновые задачи с Celery, Redis и Flower

--- src/operations/router.py
+++ src/operations/router.py
@@ -2,7 +2,7 @@ import time
 
 from fastapi import APIRouter, Depends, HTTPException
 from fastapi_cache.decorator import cache
-from sqlalchemy import select, insert
+from sqlalchemy import insert, select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from database import get_async_session
@@ -22,13 +22,13 @@ def get_long_op():
     return "Много много данных, которые вычислялись сто лет"
 
 
-@router.get("/")
+@router.get("")
 async def get_specific_operations(operation_type: str, session: AsyncSession = Depends(get_async_session)):
     try:
         query = select(operation).where(operation.c.type == operation_type)
         result = await session.execute(query)
         return {
-            "status": "scucess",
+            "status": "success",
             "data": result.all(),
             "details": None
         }
@@ -37,12 +37,15 @@ async def get_specific_operations(operation_type: str, session: AsyncSession = D
         raise HTTPException(status_code=500, detail={
             "status": "error",
             "data": None,
-            "details": None,
+            "details": None
         })
 
 
-@router.post("/")
+@router.post("")
 async def add_specific_operations(new_operation: OperationCreate, session: AsyncSession = Depends(get_async_session)):
+    """
+    docstring
+    """
     stmt = insert(operation).values(**new_operation.dict())
     await session.execute(stmt)
     await session.commit()
